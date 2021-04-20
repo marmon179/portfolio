2b73161dd8dc9a2b91a45a9c50589130ba75c34a
marmon179 <marmon179@gmail.com>
2021-04-20 20:31:06 +0300

    Block block footer fixil

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,14 +6,14 @@ export function Footer() {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Alex Lapko</h2>
+                <h4 className={style.title}>Alex Lapko</h4>
                 <div className={style.footer}>
                     <div className={style.icon}></div>
                     <div className={style.icon}></div>
                     <div className={style.icon}></div>
                     <div className={style.icon}></div>
                 </div>
-                <h4 className={style.title}>2021 Все права защищены</h4>
+                <h5 className={style.title}>2021 Все права защищены</h5>
             </div>
 
         </div>
