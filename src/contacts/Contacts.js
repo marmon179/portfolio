f5a2fd3649398fd4bf9e471f5a5845167cb46bdf
marmon179 <marmon179@gmail.com>
2021-04-30 22:50:44 +0300

    2 Contact 0,1

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import style from './Contacts.module.css'
+import style from './Contacts.module.scss'
 import styleContainer from '../common/style/Container.module.css';
+import {Title} from '../common/components/title/Title';
 
 export function Contacts() {
     return (
         <div className={style.contactsBlock}>
             <div className={`${style.contactsContainer} ${styleContainer.container}`}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title text={'Contact'}/>
                 <form className={style.contactForm}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea/>
+                    <input type="text" placeholder='Name'/>
+                    <input type="text" placeholder='E-mail'/>
+                    <textarea placeholder='Your message'/>
                     <button type="submit" className={style.submitBtn}>Отправить</button>
                 </form>
             </div>
