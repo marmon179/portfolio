b93c84f4daa22c29f7c8fff32a554ce6e8695240
marmon179 <marmon179@gmail.com>
2021-05-17 21:06:42 +0300

    2 Skill

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import style from './Skill.module.css'
+import style from './Skill.module.scss'
 
 
 export function Skill(props) {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.image}>
+
+            </div>
+            <h5>{props.title}</h5>
+            <hr className={style.hR}/>
+            <p>{props.description}</p>
         </div>
     );
 }
