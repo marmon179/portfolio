bf1c060ae04ecd05388546c5a7948589ca4d7da6
marmon179 <marmon179@gmail.com>
2021-05-17 20:29:33 +0300

    2 Contacts

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import style from './Contacts.module.scss'
-import styleContainer from '../common/style/Container.module.css';
 import {Title} from '../common/components/title/Title';
 import {Form} from './Form/Form';
 
 export function Contacts() {
     return (
         <div className={style.contactsBlock}>
-            <div className={`${style.contactsContainer} ${styleContainer.container}`}>
+            <div className={style.contactsContainer}>
                 <Title text={'Contacts'}/>
-                <Form/>
+                <div className={style.formAnData}>
+                    <Form/>
+                </div>
             </div>
 
         </div>
