0bd7ea015ffb174c001f31715b2decea8b4fc6ec
marmon179 <marmon179@gmail.com>
2021-05-18 11:21:46 +0300

    2 Header

---
+++ src/nav/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import Menu from "./menu/menu";
+import s from './Burger.module.scss'
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+export function BurgerMenu({items}) {
+    const [menuActive, setMenuActive] = useState(false)
+    return (
+        <div>
+            <div className={s.burgerBtn} onClick={() => setMenuActive(!menuActive)}>
+                <FontAwesomeIcon icon={faBars}/>
+            </div>
+            <Menu items={items} header={'menu'} active={menuActive} setActive={setMenuActive}/>
+        </div>
+    );
+}
+
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import  style from './Nav.module.css'
+import  style from './Nav.module.scss'
+import {Link} from "react-scroll";
+import {BurgerMenu} from './BurgerMenu/BurgerMenu';
 
 export  function Nav() {
+    const items = ['home','skills','portfolio','contacts']
     return (
-        <div className={style.nav}>
-            <a href="">Главная</a>
-            <a href="">Скиллы</a>
-            <a href="">Проекты</a>
-            <a href="">Контакты</a>
+        <div className={style.navigation}>
+            <div className={style.container}>
+                {items.map((item,index) =>
+                    <Link
+                        key={index}
+                        className={style.link}
+                        activeClass={style.active}
+                        to={item}
+                        spy={true}
+                        smooth={true}
+                        offset={-99}
+                        duration={500}
+                    >{item}</Link>
+                )}
+                <BurgerMenu items={items}/>
+            </div>
         </div>
     )
 }
